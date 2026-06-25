6307e39f7966beefbb99be17e5eba9ac51fd4cbd
jtaneza <j.taneza.546922@umindanao.edu.ph>
2026-06-25 18:54:58 +0800

    try again

--- obj/Release/net8.0/MalikongkongNHS.AssemblyInfo.cs
+++ obj/Release/net8.0/MalikongkongNHS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MalikongkongNHS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd073c15972bccca54ca99a68b7e141a2d9cf6d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35896d9511db069c953ecc6440a6c8e4c1775a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("MalikongkongNHS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MalikongkongNHS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
